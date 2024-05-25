6a50a10e55547d4dea56e84ec133082942c3e199
pawanrajsingh2088 <pawanrajsingh2088@gmail.com>
2024-05-25 18:15:43 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Toaster } from 'react-hot-toast';
 import './App.css';
+import "./Style/responsive.css";
 import Home from './Component/Home';
 
 function App() {
--- src/Component/Home.js
+++ src/Component/Home.js
@@ -19,7 +19,8 @@ export default function Home() {
 
     const [check,setCheck] = useState(false);
 
-    const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8684be1759368540d124f65ae398c976&units=metric`
+    console.log(process.env.REACT_APP_API_KEY);
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
 
     function convertToLocalTime(unixTimestamp) {
         // Create a Date object from the Unix timestamp (in milliseconds)
@@ -60,26 +61,27 @@ export default function Home() {
             console.log(err);
             toast.dismiss(toastId);
             toast.error("Location Not Found");
+            setCheck(false);
         }
     }
     return (
         <>
-            <div className='flex justify-center align-middle mt-20'>
-                <div className='flex flex-col items-center bg-yellow-400 h-auto p-8 w-96 rounded-3xl shadow-2xl shadow-rose-950'>
+            <div className=' head flex justify-center align-middle mt-20'>
+                <div className='main flex flex-col items-center bg-yellow-400 h-auto p-8 w-96 rounded-3xl shadow-2xl shadow-rose-950'>
                     <h1 className=' text-4xl font-bold'>Weather App</h1>
-                    <div>
+                    <div className='find'>
                         <input value={city} onChange={handleChange} type="text" placeholder='Enter Your City' className=' w-56 mt-4 h-10 border-teal-800 border-2 rounded-l-xl pl-2 font-semibold' />
                         <button onClick={apply} className=' bg-red-500 font-bold w-20 h-10 rounded-r-xl'>Search</button>
                     </div>
                     {check ?
-                        (<div className='mt-10 flex flex-col'>
-                            <h2 className=' font-medium text-2xl'><strong>Location:- </strong>{name}</h2>
-                            <h2 className=' font-medium text-2xl'><strong>Tempreture:- </strong>{temp}&deg;C</h2>
-                            <h2 className=' font-medium text-2xl'><strong>Humidity:- </strong>{humidity}%</h2>
-                            <h2 className=' font-medium text-2xl'><strong>Wind Speed:- </strong>{wind}Km/hr</h2>
-                            <h2 className=' font-medium text-2xl'><strong>Pressure:- </strong>{pressure}pascal</h2>
-                            <h2 className=' font-medium text-2xl'><strong>Sunrise:- </strong>{sunrise}</h2>
-                            <h2 className=' font-medium text-2xl'><strong>Sunset:- </strong>{sunset}</h2>
+                        (<div className=' mt-10 flex flex-col'>
+                            <h2 className='output font-medium text-2xl'><strong>Location:- </strong>{name}</h2>
+                            <h2 className='output font-medium text-2xl'><strong>Tempreture:- </strong>{temp}&deg;C</h2>
+                            <h2 className='output font-medium text-2xl'><strong>Humidity:- </strong>{humidity}%</h2>
+                            <h2 className='output font-medium text-2xl'><strong>Wind Speed:- </strong>{wind} Km/hr</h2>
+                            <h2 className='output font-medium text-2xl'><strong>Pressure:- </strong>{pressure} pascal</h2>
+                            <h2 className='output font-medium text-2xl'><strong>Sunrise:- </strong>{sunrise}</h2>
+                            <h2 className='output font-medium text-2xl'><strong>Sunset:- </strong>{sunset}</h2>
                         </div>) : 
                         ""}
                 </div>
